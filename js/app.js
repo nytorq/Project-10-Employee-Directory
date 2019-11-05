3e9eb52add2fb676e578c79f92a220e2e1f0d6f9
Charles Prator <cprator@spotx.tv>
2019-11-04 17:05:35 -0700

    Setting up & testing AJAX request
    JS
    - Created AJAX request to randomuser.com
    - Began testing out user object
    - Began organizing each card component to prepare it for rewriting
    
    SCSS/CSS
    - Line height change to <h1>
    - Renamed .profilePic to .employee-pic

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
+//
+//  VARIABLES
+//
+
 const cards = document.getElementsByClassName('card');
 const overlay = document.getElementById('overlay');
 const modal = document.getElementById('modal');
 const closeIcon = document.getElementById('closeIcon');
+let request = new XMLHttpRequest();
+let response;
+
+// AJAX Request to randomuser.com to get users!
+
+request.onreadystatechange = function () {
+  if (request.readyState === 4) {
+    response = JSON.parse(request.responseText);
+    console.log(response);
+  }
+};
+
+request.open('GET', 'https://randomuser.me/api/?results=12');
+request.send();
+
+//Event Handlers
 
 for (i=0 ; i < cards.length ; i++) {
   let card = cards[i];
@@ -26,3 +46,24 @@ overlay.addEventListener('click', ()=> {
 closeIcon.addEventListener('click', ()=> {
   closer();
 });
+
+//
+//  FUNCTIONS
+//
+
+//  Card rewriting FUNCTIONS
+
+function cardRewriter(card) {
+  let picture = card.getElementsByClassName('employee--pic');
+  let name = card.getElementsByClassName('employee--name');
+  let email = card.getElementsByClassName('employee--email');
+  let loc = card.getElementsByClassName('employee--location');
+
+  // let apiName = response.results[0].name.first;
+
+  console.log(response.results)
+
+  // name = apiName;
+}
+
+cardRewriter(cards[0]);
