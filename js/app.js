715f6724a9102de254f44b66d0ccf72d3d045b73
Charles Prator <cprator@spotx.tv>
2019-11-07 17:07:39 -0700

    All cards get populated with API data
    - Finished cardRewriter() so that it populates all the cards on the page
    - Began work on the modal to populate its information with whatever card you clicked
    - Realized I needed to create an object that stored all the user information

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 //
 //  VARIABLES
 //
+const users = [
+  {
+    firstName: '',
+    lastName: '',
+    fullName: function () {
+      firstName.concat(" ", lastName);
+    },
+    email: '',
+    phone: '',
+    birthday: '',
+    location: {
+      city: '',
+      state: '',
+      country: '',
+      street: {
+        number: null,
+        name: '',
+      },
+      zipcode: null
+    },
+    picture: ''
+  }
+
+];
 
 const cards = document.getElementsByClassName('card');
 const overlay = document.getElementById('overlay');
@@ -9,22 +33,48 @@ const closeIcon = document.getElementById('closeIcon');
 let request = new XMLHttpRequest();
 let response;
 
+function cardRewriter(card) {
+  let picture = card.getElementsByClassName('employee--pic');
+  let name = card.getElementsByClassName('employee--name');
+  let email = card.getElementsByClassName('employee--email');
+  let loc = card.getElementsByClassName('employee--location');
+  let apiFirstName = response.results[i].name.first;
+  let apiLastName = response.results[i].name.last;
+  let apiEmail = response.results[i].email
+  let apiCity = response.results[i].location.city;
+  let apiPicture = response.results[i].picture.medium;
+  name[0].innerText = apiFirstName.concat(" ", apiLastName);
+  name[0].innerText = apiFirstName.concat(" ", apiLastName);
+  email[0].innerText = apiEmail;
+  loc[0].innerText = apiCity;
+  picture[0].src = apiPicture;
+}
+
+function modalPopulator(modal, target) {
+  let picture = modal.getElementsByClassName('employee--pic');
+  let name = modal.getElementsByClassName('employee--name');
+  let email = modal.getElementsByClassName('employee--email');
+  let loc = modal.getElementsByClassName('employee--location');
+  let apiFirstName = response.results[i].name.first;
+  let apiLastName = response.results[i].name.last;
+  let apiEmail = response.results[i].email
+  let apiCity = response.results[i].location.city;
+  let apiPicture = response.results[i].picture.medium;
+  name[0].innerText = apiFirstName.concat(" ", apiLastName);
+  name[0].innerText = apiFirstName.concat(" ", apiLastName);
+  email[0].innerText = apiEmail;
+  loc[0].innerText = apiCity;
+  picture[0].src = apiPicture;
+}
+
 // AJAX Request to randomuser.com to get users!
 
 request.onreadystatechange = function () {
   if (request.readyState === 4) {
     response = JSON.parse(request.responseText);
-    console.log(response);
-    function cardRewriter(card) {
-      let picture = card.getElementsByClassName('employee--pic');
-      let name = card.getElementsByClassName('employee--name');
-      let email = card.getElementsByClassName('employee--email');
-      let loc = card.getElementsByClassName('employee--location');
-      let apiName = response.results[0].name.first;
-      name[0].innerText = apiName;
+    for (i=0 ; i<cards.length ; i++) {
+      cardRewriter(cards[i]);
     }
-
-    cardRewriter(cards[0]);
   }
 };
 
@@ -38,6 +88,7 @@ for (i=0 ; i < cards.length ; i++) {
   card.addEventListener('click', ()=> {
     overlay.style.visibility = 'visible';
     modal.style.visibility = 'visible';
+    cardRewriter(modal);
   });
 }
 
