2d01da134e65abd06bd4ee097d4cb885ee7d52d3
Charles Prator <cprator@spotx.tv>
2019-11-14 17:07:28 -0700

    Fixed cardWriter() to accept modal now
    - Removed users object
    - Fixed cardwriter() to correclty distinguish between cards and modal via an if() statement

--- js/app.js
+++ js/app.js
@@ -2,31 +2,6 @@
 //  VARIABLES
 //
 
-// const users = [
-//   {
-//     firstName: '',
-//     lastName: '',
-//     fullName: function () {
-//       firstName.concat(" ", lastName);
-//     },
-//     email: '',
-//     phone: '',
-//     birthday: '',
-//     location: {
-//       city: '',
-//       state: '',
-//       country: '',
-//       street: {
-//         number: null,
-//         name: '',
-//       },
-//       zipcode: null
-//     },
-//     picture: ''
-//   }
-//
-// ];
-
 const cards = document.getElementsByClassName('card');
 const overlay = document.getElementById('overlay');
 const modal = document.getElementById('modal');
@@ -35,38 +10,39 @@ const typeAhead = document.getElementById('typeAhead');
 let request = new XMLHttpRequest();
 let response;
 
-// function cardWriter(object, i = 0) {
-//   let picture;
-//   let name;
-//   let email;
-//   let loc;
-//   if (i === null) {
-//     picture = object.getElementsByClassName('employee--pic');
-//     name = object.getElementsByClassName('employee--name');
-//     email = object.getElementsByClassName('employee--email');
-//     loc = object.getElementsByClassName('employee--location');
-//   } else {
-//     picture = object[i].getElementsByClassName('employee--pic');
-//     name = object[i].getElementsByClassName('employee--name');
-//     email = object[i].getElementsByClassName('employee--email');
-//     loc = object[i].getElementsByClassName('employee--location');
-//   }
-//   let apiPicture = response.results[i].picture.medium;
-//   let apiFirstName = response.results[i].name.first;
-//   let apiLastName = response.results[i].name.last;
-//   let apiEmail = response.results[i].email
-//   let apiCity = response.results[i].location.city;
-//   picture[0].src = apiPicture;
-//   name[0].innerText = apiFirstName.concat(" ", apiLastName);
-//   email[0].innerText = apiEmail;
-//   loc[0].innerText = apiCity;
-//   if (object[i].lastElementChild.className.includes('birthday')) {
-//     let phone = object.getElementsByClassName('employee--phone');
-//     let apiPhone = response.results[i].cell;
-//     console.log(phone);
-//     phone[0].innerText = apiPhone;
-//   }
-// }
+function cardWriter(object, i = 0) {
+  let picture;
+  let name;
+  let email;
+  let loc;
+  if (object === cards) {
+    picture = object[i].getElementsByClassName('employee--pic');
+    name = object[i].getElementsByClassName('employee--name');
+    email = object[i].getElementsByClassName('employee--email');
+    loc = object[i].getElementsByClassName('employee--location');
+  } else {
+    picture = object.getElementsByClassName('employee--pic');
+    name = object.getElementsByClassName('employee--name');
+    email = object.getElementsByClassName('employee--email');
+    loc = object.getElementsByClassName('employee--location');
+    let phone = object.getElementsByClassName('employee--phone');
+    let apiPhone = response.results[i].cell;
+    phone[0].innerText = apiPhone;
+    let birthday = object.getElementsByClassName('employee--birthday');
+    let apiDOB = response.results[i].dob.date;
+    apiDOB = new Date(apiDOB);
+    birthday[0].innerText = apiDOB;
+  }
+  let apiPicture = response.results[i].picture.medium;
+  let apiFirstName = response.results[i].name.first;
+  let apiLastName = response.results[i].name.last;
+  let apiEmail = response.results[i].email
+  let apiCity = response.results[i].location.city;
+  picture[0].src = apiPicture;
+  name[0].innerText = apiFirstName.concat(" ", apiLastName);
+  email[0].innerText = apiEmail;
+  loc[0].innerText = apiCity;
+}
 
 // function modalPopulator(modal, target) {
 //   let picture = modal.getElementsByClassName('employee--pic');
@@ -87,19 +63,22 @@ let response;
 
 // AJAX Request to randomuser.com to get users!
 
-// request.onreadystatechange = function () {
-//   if (request.readyState === 4) {
-//     response = JSON.parse(request.responseText);
-//     for (i=0 ; i<cards.length ; i++) {
-//       cardWriter(cards, i);
-//     }
-//   }
-// };
-//
-// request.open('GET', 'https://randomuser.me/api/?results=12');
-// request.send();
+request.onreadystatechange = function () {
+  if (request.readyState === 4) {
+    response = JSON.parse(request.responseText);
+    for (i=0 ; i<cards.length ; i++) {
+      cardWriter(cards, i);
+    }
+  }
+};
 
-//Event Handlers
+request.open('GET', 'https://randomuser.me/api/?results=12');
+request.send();
+
+
+//
+//  Event Handlers
+//
 
 for (let i=0 ; i < cards.length ; i++) {
   cards[i].addEventListener('click', ()=> {
